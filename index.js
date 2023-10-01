578c9d8f1ba4deb4affedf81389b786f2597a1da
Jazper <jazpersaldana@yahoo.com>
2023-10-01 17:22:03 -0500

    dom modifying elements lab complete

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove()
+const newHeader = document.createElement('h1')
+newHeader.id = "victory";
+newHeader.textContent = "Luca is the champion"
+document.body.append('newHeader')
